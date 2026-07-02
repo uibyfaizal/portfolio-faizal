7ff7fe4a4c9be6d652a89cb2b0625ea58196ee2a
uibyfaizal <muh.rahmat.faisal.047@gmail.com>
2026-07-02 16:50:16 +0800

    Fix CSS import

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 // MAIN CSS
-import '../src/dist/main.css'
+import './main.css'
 import App from './App.jsx'
 
 // Font Poppins
